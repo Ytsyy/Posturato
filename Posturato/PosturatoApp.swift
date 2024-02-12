99ce3785eb75ee9d077c28fa40bdc1351161b597
Maxim <Maxim@MacBook-Air-test.local>
2024-02-12 11:40:14 +0700

    Create project

---
+++ Posturato/PosturatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosturatoApp.swift
+//  Posturato
+//
+//  Created by Maxim on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PosturatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
