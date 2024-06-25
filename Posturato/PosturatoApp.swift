dcae794c6436be4e8c53b295b4f76b0514826062
Maxim <marin.maxim.work@gmail.com>
2024-06-25 14:06:44 +0700

    Add firebase settings

--- Posturato/PosturatoApp.swift
+++ Posturato/PosturatoApp.swift
@@ -6,13 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PosturatoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContainerView()
                 .background(Color("LightBeige")).ignoresSafeArea(.all)
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
